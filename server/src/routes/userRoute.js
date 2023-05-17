68ea557c63c096f7975be26ba559ff379ec0a9e3
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-17 13:44:28 +0800

    Update userRoute.js

--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -6,12 +6,16 @@ import {
   registerUser,
   updateUserProfile,
 } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const userRoute = Router();
 
 userRoute.post("/", registerUser);
 userRoute.post("/auth", authUser);
 userRoute.post("/logout", logoutUser);
-userRoute.route("/profile").get(getUserProfile).put(updateUserProfile)
+userRoute
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default userRoute;
