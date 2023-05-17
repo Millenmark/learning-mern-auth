3ddda43a7e5e64399e291e2f1bffebffe7213607
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-17 11:11:35 +0800

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoute from "./routes/userRoute.js";
+import connectToDatabase from "./config/database.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 dotenv.config();
-
+connectToDatabase()
 const port = process.env.PORT || 5000;
-
 const app = express();
 
 //middleware
 app.use(express.json());
-app.use(notFound);
-app.use(errorHandler);
+// app.use(notFound);
+// app.use(errorHandler);
 
 //routes
 app.use("/api/users", userRoute);
