8862997ff96298fe9cd95c4036d101e95d4ba643
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-17 11:30:58 +0800

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,25 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import userRoute from "./routes/userRoute.js";
 import connectToDatabase from "./config/database.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
-connectToDatabase()
+connectToDatabase();
 const port = process.env.PORT || 5000;
 const app = express();
 
-//middleware
+//middleware before the routes
 app.use(express.json());
-// app.use(notFound);
-// app.use(errorHandler);
+app.use(cors());
+
 
 //routes
 app.use("/api/users", userRoute);
+
+//middleware after the routes
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log("Server is already running..."));
