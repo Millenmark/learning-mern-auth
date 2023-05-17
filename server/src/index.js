3b383ec836b1c21847f067a76349709051a33afd
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-17 13:44:19 +0800

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser"
 import userRoute from "./routes/userRoute.js";
 import connectToDatabase from "./config/database.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -12,8 +13,9 @@ const app = express();
 
 //middleware before the routes
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
+app.use(cookieParser())
 
 //routes
 app.use("/api/users", userRoute);
