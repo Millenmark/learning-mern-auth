2a06ae254435136e69625ab3a4452430964932ac
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-21 16:41:42 +0800

    Update ProfileScreen.jsx

--- client/src/screens/ProfileScreen.jsx
+++ client/src/screens/ProfileScreen.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,7 +13,7 @@ const ProfileScreen = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const navigate = useNavigate();
+
   const dispatch = useDispatch();
 
   const { userInfo } = useSelector((state) => state.auth);
